ba90e8f3edb9c8e694b4dfd17c3fcc30b862306c
Jason Allum <jallum@tonicdesign.com>
2018-07-02 10:24:49 -0400

    Adjusted dependencies.

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     ],
     targets: [
         .target(name: "Clibnvinfer", dependencies: []),
-        .target(name: "ClibTensorRT", dependencies: ["Clibnvinfer"]),
-        .target(name: "TensorRT", dependencies: ["Clibnvinfer", "ClibTensorRT", "Clibcuda"]),
+        .target(name: "ClibTensorRT", dependencies: ["Clibnvinfer", "Clibcuda"]),
+        .target(name: "TensorRT", dependencies: ["ClibTensorRT"]),
     ],
     cxxLanguageStandard: .cxx11
 )
