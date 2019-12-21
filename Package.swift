5bbf5cebd77398a406e0fa9b202f4b41fdf0640b
Jason Allum <jason@slyce.it>
2019-12-20 21:41:04 -0500

    Updated to use new public versions.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "TensorRT",
     products: [ .library(name: "TensorRT", targets: ["TensorRT"]) ],
     dependencies: [
-        .package(url: "git@github.com:Slyce-Inc/swift-cuda.git", .upToNextMinor(from: "1.0.0")),
+        .package(url: "http://github.com/Slyce-Inc/swift-cuda.git", .upToNextMinor(from: "1.0.0")),
     ],
     targets: [
         .target(name: "Clibnvinfer", dependencies: []),
